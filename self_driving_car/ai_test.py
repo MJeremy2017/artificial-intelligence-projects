da63ffd00e121fdf7831677aa3392ea8826207b4
jeremy.zhang <zhangyue9306@gmail.com>
2020-06-03 15:10:08 +0800

    add ai test

--- self_driving_car/ai_test.py
+++ self_driving_car/ai_test.py
@@ -58,12 +58,12 @@ class Dqn(object):
 
     def select_action(self,  state):
         # softmax([1, 2, 3]) => (0.11, 0.15, 0.74) | softmax([1, 2, 3]*3) => (0.0, 0.1, 0.9)
-        probs = F.softmax(self.model(Variable(state, volatile=True)) * 100)  # Temperature = 100
+        probs = F.softmax(self.model(Variable(state, volatile=True)) * 100)  # Temperature = 100 | 0 to deactivate
         # randomly select an action
         action = probs.multinomial()
-        return action.data([0, 0])
+        return action.data[0, 0]
 
-    def learn(self, batch_state, batch_action, batch_reward, batch_next_state):
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
         # https://stackoverflow.com/questions/50999977/what-does-the-gather-function-do-in-pytorch-in-layman-terms
         # only pickup action that is acted
         # 5 x 100 => model => 3 x 100 => gather => 100 x 1 => squeeze => 100,
@@ -76,6 +76,48 @@ class Dqn(object):
         loss.backward(retain_variables=True)
         # update weights
         self.optimizer.step()
+
+    def update(self, reward, new_signal):
+        new_state = torch.Tensor(new_signal).float().unsqueeze(0)
+        # long tensor only accept int type
+        self.memory.push(
+            (self.last_state, new_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
+        # update dqn
+        if len(self.memory.memory) > 100:
+            batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(100)
+            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
+
+        action = self.select_action(new_state)
+        self.last_action = action
+        self.last_state = new_state
+        self.last_reward = reward
+        self.reward_window.append(reward)
+        if len(self.reward_window) > 1000:
+            del self.reward_window[0]
+        return action
+
+    def score(self):
+        return sum(self.reward_window)/(1. + len(self.reward_window))
+
+    # save model in python dict
+    def save(self):
+        # state_dict: layer -> parameters
+        torch.save({
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict()
+        }, 'last_brain.pth')
+
+    def load(self):
+        file_name = 'last_brain.pth'
+        if os.path.isfile(file_name):
+            print("loading model ...")
+            checkpoint = torch.load(file_name)
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print("done!")
+        else:
+            print("file does not exist!")
+
 
 
 
