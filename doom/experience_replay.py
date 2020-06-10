28784e72700e9c21265d6eb77714062f0ddbbbdc
jeremy.zhang <zhangyue9306@gmail.com>
2020-06-10 12:20:26 +0800

    reformating

--- doom/experience_replay.py
+++ doom/experience_replay.py
@@ -7,10 +7,9 @@ from collections import namedtuple, deque
 # Defining one Step
 Step = namedtuple('Step', ['state', 'action', 'reward', 'done'])
 
-# Making the AI progress on several (n_step) steps
 
+# Making the AI progress on several (n_step) steps
 class NStepProgress:
-    
     def __init__(self, env, ai, n_step):
         self.ai = ai
         self.rewards = []
@@ -25,7 +24,8 @@ class NStepProgress:
             action = self.ai(np.array([state]))[0][0]
             next_state, r, is_done, _ = self.env.step(action)
             reward += r
-            history.append(Step(state = state, action = action, reward = r, done = is_done))
+            # record each state, action and reward
+            history.append(Step(state=state, action=action, reward=r, done=is_done))
             while len(history) > self.n_step + 1:
                 history.popleft()
             if len(history) == self.n_step + 1:
@@ -47,17 +47,16 @@ class NStepProgress:
         self.rewards = []
         return rewards_steps
 
-# Implementing Experience Replay
 
+# Implementing Experience Replay
 class ReplayMemory:
-    
-    def __init__(self, n_steps, capacity = 10000):
+    def __init__(self, n_steps, capacity=10000):
         self.capacity = capacity
         self.n_steps = n_steps
         self.n_steps_iter = iter(n_steps)
         self.buffer = deque()
 
-    def sample_batch(self, batch_size): # creates an iterator that returns random batches
+    def sample_batch(self, batch_size):  # creates an iterator that returns random batches
         ofs = 0
         vals = list(self.buffer)
         np.random.shuffle(vals)
@@ -67,8 +66,8 @@ class ReplayMemory:
 
     def run_steps(self, samples):
         while samples > 0:
-            entry = next(self.n_steps_iter) # 10 consecutive steps
-            self.buffer.append(entry) # we put 200 for the current episode
+            entry = next(self.n_steps_iter)  # 10 consecutive steps
+            self.buffer.append(entry)  # we put 200 for the current episode
             samples -= 1
-        while len(self.buffer) > self.capacity: # we accumulate no more than the capacity (10000)
+        while len(self.buffer) > self.capacity:  # we accumulate no more than the capacity (10000)
             self.buffer.popleft()
